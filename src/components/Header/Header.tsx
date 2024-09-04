fe9396a4ebe23025f509bf09dee60ad6e4bc2612
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-04 20:30:58 +0300

    Update
    Updated for Header styles.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,33 +15,35 @@ const Header: React.FC = () => {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image
-          src="/img/logo.svg"
-          alt="Easybank Logo"
-          width={150}
-          height={40}
-        />
-      </div>
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <Image
-          src={menuOpen ? "/img/icon-close.svg" : "/img/icon-hamburger.svg"}
-          alt="Menu Icon"
-          width={24}
-          height={24}
-        />
-      </div>
-      <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ""}`}>
-        <ul className={styles.menu}>
-          {menuItems.map((item) => (
-            <li key={item.id} className={styles.menuItem}>
-              <Link href={item.href}>{item.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className={styles.cta}>
-        <button className={styles.inviteButton}>Request Invite</button>
+      <div className={`container mx-auto px-4 ${styles.header_content}`}>
+        <div className={styles.logo}>
+          <Image
+            src="/img/logo.svg"
+            alt="Easybank Logo"
+            width={150}
+            height={40}
+          />
+        </div>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <Image
+            src={menuOpen ? "/img/icon-close.svg" : "/img/icon-hamburger.svg"}
+            alt="Menu Icon"
+            width={24}
+            height={24}
+          />
+        </div>
+        <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ""}`}>
+          <ul className={styles.menu}>
+            {menuItems.map((item) => (
+              <li key={item.id} className={styles.menuItem}>
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.cta}>
+          <button className={styles.inviteButton}>Request Invite</button>
+        </div>
       </div>
     </header>
   );
