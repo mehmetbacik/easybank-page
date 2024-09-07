55434cb20904ba5640df4a40f7dc404e3dd32a17
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-09-08 00:30:40 +0300

    Update
    Updated for Articles styles.

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -6,25 +6,27 @@ import { articles } from "@/data/articlesData";
 const Articles: React.FC = () => {
   return (
     <section className={styles.articles}>
-      <h2 className={styles.title}>Latest Articles</h2>
-      <div className={styles.cards}>
-        {articles.map((article, index) => (
-          <div key={index} className={styles.card}>
-            <div className={styles.imageWrapper}>
-              <Image
-                src={article.image}
-                alt={article.title}
-                width={350}
-                height={233}
-              />
+      <div className={`container mx-auto px-4 ${styles.content}`}>
+        <h2 className={styles.title}>Latest Articles</h2>
+        <div className={styles.cards}>
+          {articles.map((article, index) => (
+            <div key={index} className={styles.card}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={article.image}
+                  alt={article.title}
+                  width={350}
+                  height={233}
+                />
+              </div>
+              <div className={styles.cardContent}>
+                <p className={styles.author}>By {article.author}</p>
+                <h3 className={styles.cardTitle}>{article.title}</h3>
+                <p className={styles.description}>{article.description}</p>
+              </div>
             </div>
-            <div className={styles.cardContent}>
-              <p className={styles.author}>By {article.author}</p>
-              <h3 className={styles.cardTitle}>{article.title}</h3>
-              <p className={styles.description}>{article.description}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
